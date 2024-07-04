974fedc1718790b72b8f89b41c77e7099538e5fc
Pedro AV <97904358+PedroArvina@users.noreply.github.com>
2024-07-04 16:44:22 -0300

    Delete Batalhas/src/Menu directory

--- Batalhas/src/Menu/Instrucoes.java
+++
@@ -1,8 +0,0 @@
-package Menu;
-
-public class Instrucoes {
-    public static void instrucoes() {
-        System.out.println("Mostrar instruções");
-        // Aqui você irá implementar a lógica para mostrar as instruções do jogo.
-    }
-}
