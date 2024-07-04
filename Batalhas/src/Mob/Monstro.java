6d2d8a20ef6c8126ea34186083c6c51114adfd5c
Pedro AV <97904358+PedroArvina@users.noreply.github.com>
2024-07-04 16:44:31 -0300

    Delete Batalhas/src/Mob directory

--- Batalhas/src/Mob/Monstro.java
+++
@@ -1,7 +0,0 @@
-package Mob;
-
-public abstract class Monstro extends Personagem {
-    public Monstro(String nome, int hp, int ataque, int range, int alcanceMovimento) {
-        super(nome, hp, ataque, range, alcanceMovimento);
-    }
-}
